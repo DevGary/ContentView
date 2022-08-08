735e2df26770a5aab29d5848f48d11562c8aa315
Gary <ImDevGary@gmail.com>
2022-08-08 04:33:32 -0700

    Add files for Demo Empty Activity

---
+++ demo/src/main/java/com/devgary/contentviewdemo/DemoActivity.kt
@@ -0,0 +1,12 @@
+package com.devgary.contentviewdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devgary.contentview.R
+
+class DemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo)
+    }
+}
