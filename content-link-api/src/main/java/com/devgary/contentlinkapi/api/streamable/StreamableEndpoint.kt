7b21e8881ca2067ffb7349e032cf5dc15f7ec2f8
Gary <ImDevGary@gmail.com>
2022-08-13 16:37:34 -0700

    Convert Streamable API from RxJava to Coroutines (#7)
    Use Coroutines instead of RxJava because it is simpler and doesn't need add as many dependencies to library

--- content-link-api/src/main/java/com/devgary/contentlinkapi/api/streamable/StreamableEndpoint.kt
+++ content-link-api/src/main/java/com/devgary/contentlinkapi/api/streamable/StreamableEndpoint.kt
@@ -1,10 +1,9 @@
 package com.devgary.contentlinkapi.api.streamable
 
-import io.reactivex.rxjava3.core.Observable
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface StreamableEndpoint {
     @GET("/videos/{shortcode}")
-    fun getVideo(@Path("shortcode") shortCode: String): Observable<StreamableVideoResponse>
+    suspend fun getVideo(@Path("shortcode") shortCode: String): StreamableVideoResponse
 }
