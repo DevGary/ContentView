86227cc5a849e62ca924793bd5730ffcafa1ef13
Gary <ImDevGary@gmail.com>
2022-08-13 08:21:15 -0700

    Implement Streamable API support (#7)

---
+++ content-link-api/src/main/java/com/devgary/contentlinkapi/api/streamable/StreamableVideoResponse.kt
@@ -0,0 +1,13 @@
+package com.devgary.contentlinkapi.api.streamable
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class StreamableVideoResponse(
+    var url: String,
+    @Json(name = "thumbnail_url")
+    var thumbnailUrl: String? = null,
+    var title: String? = null,
+    var videos: MutableList<StreamableVideo> = mutableListOf()
+)
--- demo/src/main/java/com/devgary/contentviewdemo/DemoActivity.kt
+++ demo/src/main/java/com/devgary/contentviewdemo/DemoActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import com.devgary.contentcore.model.Content
+import com.devgary.contentcore.model.ContentType
+import com.devgary.contentlinkapi.api.streamable.StreamableApi
 import com.devgary.contentview.R
 import com.devgary.contentview.databinding.ActivityDemoBinding
 
@@ -25,6 +28,23 @@ class DemoActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_streamable -> {
+                StreamableApi().getVideos("hn8hq").subscribe { response ->
+                    response.let {
+                        val video = response.videos
+                            .filter { v -> v.url.isNotEmpty() }
+                            .minByOrNull { v -> v.bitrate }
+
+                        video?.let {
+                            binding.contentview.showContent(Content(video.url, ContentType.VIDEO))
+                        }
+                    }
+                }
+                return true
+            }
+        }
+        
         when(item.itemId) {
             R.id.menu_image -> SampleContent.IMAGE_CONTENT
             R.id.menu_gif -> SampleContent.GIF_CONTENT
