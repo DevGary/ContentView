7600157ce3385990bfbeccf01f36b51a276a15b0
Gary <ImDevGary@gmail.com>
2022-08-22 21:14:41 -0700

    Refactor content-link-api code to be more testable (#20)

--- content-link-api/src/main/java/com/devgary/contentlinkapi/components/streamable/api/StreamableClient.kt
+++ content-link-api/src/main/java/com/devgary/contentlinkapi/components/streamable/api/StreamableClient.kt
@@ -4,32 +4,9 @@ import android.util.Log
 import com.devgary.contentcore.util.TAG
 import com.devgary.contentcore.util.name
 import com.devgary.contentlinkapi.components.streamable.api.model.StreamableVideoResponse
-import com.devgary.contentlinkapi.components.streamable.api.model.StreamableVideoResponseAdapter
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
-class StreamableClient {
-    private val BASE_URL = "https://api.streamable.com/"
-    
+class StreamableClient(private val streamableEndpoint: StreamableEndpoint) {
     private val cachedStreamableVideoResponses: MutableMap<String, StreamableVideoResponse> = HashMap()
-    private val streamableEndpoint: StreamableEndpoint
-
-    init {
-        val moshi = Moshi.Builder()
-            .add(StreamableVideoResponseAdapter())
-            .add(KotlinJsonAdapterFactory())
-            .build()
-        
-        streamableEndpoint = Retrofit.Builder()
-            .client(OkHttpClient())
-            .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
-            .create(StreamableEndpoint::class.java)
-    }
 
     suspend fun getVideo(shortCode: String): StreamableVideoResponse {
         cachedStreamableVideoResponses[shortCode]?.let {
