afb71707f7eb0df90d92359e1ff511b1ca607a10
Gary <ImDevGary@gmail.com>
2023-04-15 09:36:06 -0700

    Tweak ActivatableContent logString

--- core/content-core/src/main/java/com/devgary/contentcore/model/content/ActivatableContent.kt
+++ core/content-core/src/main/java/com/devgary/contentcore/model/content/ActivatableContent.kt
@@ -2,3 +2,8 @@ package com.devgary.contentcore.model.content
 
 class ActivatableContent(val contentWhenNotActivated: Content, val contentWhenActivated: Content) :
     Content(contentWhenNotActivated.source, contentWhenNotActivated.type)
+{
+    override fun toLogString() =
+        "Unactivated: ${contentWhenNotActivated.type} ${contentWhenNotActivated.source.toLogString()}, " +
+                "Activated: ${contentWhenActivated.type} ${contentWhenActivated.source.toLogString()}"
+}
